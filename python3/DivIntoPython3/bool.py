40df275e7c698bd248e50826a2dd75c12ea7fe15
Liu Ming <miliu@statoil.com>
2013-09-06 22:15:55 +0800

    Test bool type of python3.

---
+++ python3/DivIntoPython3/bool.py
@@ -0,0 +1,17 @@
+def is_it_true(anything):
+	'''this function test the bool
+		
+		variable of python3
+	'''
+	if anything:
+		print(anything, "yes, it's true")
+	else:
+		print(anythng, "no, it's false")
+
+if __name__ == '__main__':
+	is_it_true(1)
+	is_it_true(0)
+	is_it_true(2)
+	is_it_true(-1)
+	
+	
