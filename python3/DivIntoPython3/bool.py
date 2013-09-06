ca5a002f988d116350ad2fd18b85066e3fd798bb
Liu Ming <miliu@statoil.com>
2013-09-06 22:53:22 +0800

    Test bool for list

--- python3/DivIntoPython3/bool.py
+++ python3/DivIntoPython3/bool.py
@@ -6,7 +6,7 @@ def is_it_true(anything):
 	if anything:
 		print(anything, "yes, it's true")
 	else:
-		print(anythng, "no, it's false")
+		print(anything, "no, it's false")
 
 if __name__ == '__main__':
 	is_it_true(1)
@@ -14,4 +14,7 @@ if __name__ == '__main__':
 	is_it_true(2)
 	is_it_true(-1)
 	
-	
+	is_it_true([])
+	is_it_true(['a', 'c'])
+	is_it_true([False])
+
