fbf883ef4898e1b88fa797d517c5c75ec1da5e50
Liu Ming <miliu@statoil.com>
2014-03-20 21:02:54 +0800

    fix print function problem.

--- python3/scripts/fit.py
+++ python3/scripts/fit.py
@@ -17,7 +17,7 @@ pl.figure()
 pl.plot(x, y, 'bo',label="true data")
 p0 = [1, 1, 1, 1] 
 plsq = leastsq(residuals, p0, args=(x, y))
-print  plsq[0] 
+print(plsq[0])
 
 pl.plot(x, func(x, plsq[0]), 'g+', label="fit data")
 pl.legend()
